4f8bd3f36e528f8b3c03c44124460e283d7b13a8
Tilen Marš <its0black0magic@gmail.com>
2023-11-22 11:15:03 +0000

    Set menu clicks to call functions instead of links

--- src/index.js
+++ src/index.js
@@ -2,5 +2,10 @@ import { loadIndex } from './indexPage.js';
 import {renderMenu} from './menu.js';
 import {loadContact} from './contact.js';
 //loadIndex();
-//renderMenu();
-//loadContact();
+const homeButton = document.getElementById('home');
+homeButton.addEventListener('click', loadIndex);
+const menuButton = document.getElementById('menu');
+menuButton.addEventListener('click', renderMenu);
+const contactButton = document.getElementById('contact');
+contactButton.addEventListener('click', loadContact);
+
